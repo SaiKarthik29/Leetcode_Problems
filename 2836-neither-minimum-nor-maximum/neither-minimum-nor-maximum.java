429b9af059ed8db2fe51cf945201db405c08f953
Sriram Sai Karthik <mail2sriramsaikarthik@gmail.com>
2026-02-13 23:41:13 +0530

    Time: 4 ms (100.00%) | Memory: 47 MB (85.00%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
@@ -3,7 +3,23 @@ class Solution {
         if(nums.length<=2){
             return -1;
         }
-        Arrays.sort(nums);
-        return nums[1];
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<min){
+                min=nums[i];
+            }
+            if(nums[i]>max){
+                max=nums[i];
+            }
+        }
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=min && nums[i]!=max){
+                res=nums[i];
+                break;
+            }
+        }
+        return res;
     }
 }
