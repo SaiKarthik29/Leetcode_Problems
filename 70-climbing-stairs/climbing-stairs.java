69b0be3e8015ce8abfd3eda82ddfdaf9b1543bf6
Sriram Sai Karthik <mail2sriramsaikarthik@gmail.com>
2026-05-03 10:32:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (53.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,12 +3,12 @@ class Solution {
         if(n<=2){
             return n;
         }
-        int way2=1,way1=2;
-        for(int i=2;i<n;i++){
-            int curr=way1+way2;
-            way2=way1;
-            way1=curr;
+        int a=1,b=2;
+        for(int i=3;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
         }
-        return way1;
+        return b;
     }
 }
